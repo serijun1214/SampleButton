e7c5300c9bb6adf73028fb38384dd79ab0b15c1e
serijun1214 <serijun.1214+github@gmail.com>
2024-09-09 09:09:06 +0900

    Initial Commit

---
+++ SampleButton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleButton
+//
+//  Created by 瀬利純樹 on 2024/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
